4718587d475361dbf115f36a0f66f98d0ebf3e96
wojciech.blachowski <wblachowsky@gmail.com>
2022-01-16 21:16:23 +0100

    Fix hprange

--- utils.py
+++ utils.py
@@ -6,4 +6,4 @@ from hyperopt import hp
 def hprange(name, start, stop, step=1):
     interval = stop - start
     stop = math.ceil(interval / step)
-    return start + hp.randint(name, stop + 1) * step
+    return start + hp.randint(name, stop) * step
